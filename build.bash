f96b4c4cb80ff4edd8da7a382864a32b79a086c9
Raphael Zimmermann <mail@raphael.li>
2018-03-19 08:15:03 +0100

    Add compodoc

--- build.bash
+++ build.bash
@@ -4,6 +4,7 @@ set -exu
 npm install
 npm run ng -- lint
 npm run ng -- test --single-run --no-progress --code-coverage --browsers ChromeHeadlessNoSandbox
+npm run compodoc
 npm run ng -- build --prod --aot --no-progress
 
 
