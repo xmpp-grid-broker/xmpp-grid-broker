ebe412c22e822c5bdabbb0ad3efba133fdef7914
Raphael Zimmermann <mail@raphael.li>
2018-03-23 16:39:53 +0100

    Add list & spinner

--- src/app/features/topic-overview/root-topics/root-topics.component.ts
+++ src/app/features/topic-overview/root-topics/root-topics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root-topics',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RootTopicsComponent implements OnInit {
 
-  constructor() { }
+  isLoaded = false;
+  nodes: [{ title: string }] = [{title: 'Node #1'}, {title: 'Node #2'}, {title: 'Node #3'}];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 800);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LayoutsModule} from './layouts/layouts.module';
+import {ListComponent} from './list/list.component';
+import {ListItemComponent} from './list/list-item/list-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LayoutsModule
   ],
-  exports: [LayoutsModule],
-  declarations: [],
+  declarations: [ListComponent, ListItemComponent],
+  exports: [LayoutsModule, ListComponent, ListItemComponent],
 })
 export class SharedModule {
 }
