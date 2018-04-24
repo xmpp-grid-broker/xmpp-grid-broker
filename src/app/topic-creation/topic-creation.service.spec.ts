500db38acb659c875d126dba90b5448bdb113178
Raphael Zimmermann <mail@raphael.li>
2018-04-24 16:30:23 +0200

    Load default config if available for topic creation

--- src/app/topic-creation/topic-creation.service.spec.ts
+++ src/app/topic-creation/topic-creation.service.spec.ts
@@ -29,12 +29,13 @@ describe('TopicCreationService', () => {
 
   it('should call `createTopic` on the client', (done) => {
     spyOn(client, 'createNode').and.callThrough();
-    service.createTopic('testing')
+    service.createTopic('testing', null)
       .then((generatedTopicTitle) => {
         expect(generatedTopicTitle).toBe('testing');
         expect(client.createNode).toHaveBeenCalledTimes(1);
         const args = client.createNode.calls.mostRecent().args;
         expect(args[1]).toBe('testing');
+        console.log(args[2]);
         expect(Object.keys(args[2]).length).toBe(0);
         done();
       }).catch((error) => {
@@ -44,7 +45,7 @@ describe('TopicCreationService', () => {
 
   it('should call `createTopic` with `true` if the given value is falsy', (done) => {
     spyOn(client, 'createNode').and.callThrough();
-    service.createTopic(null)
+    service.createTopic(null, null)
       .then((generatedTopicTitle) => {
         expect(generatedTopicTitle).toBe('generatedNodeID');
         expect(client.createNode).toHaveBeenCalledTimes(1);
@@ -61,7 +62,7 @@ describe('TopicCreationService', () => {
     spyOn(client, 'createNode').and.callFake((jid, title, config, cb) => {
       cb({error: {condition: 'conflict'}});
     });
-    service.createTopic(null)
+    service.createTopic(null, null)
       .then(() => {
         fail(`Expected an error instead of a successful result!`);
       })
--- src/app/topic-creation/topic-creation/topic-creation.component.ts
+++ src/app/topic-creation/topic-creation/topic-creation.component.ts
@@ -3,6 +3,9 @@ import {TopicCreationErrors, TopicCreationService} from '../topic-creation.servi
 import {NavigationService} from '../../core/navigation.service';
 import {FormControl, FormGroup} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
+import {FormProcessingStatus} from '../../shared/FormProcessingStatus';
+import {XmppDataForm} from '../../core/models/FormModels';
+import {TopicConfigComponent} from '../../topic-widgets/topic-config/topic-config.component';
 
 @Component({
   selector: 'xgb-topic-creation',
@@ -11,8 +14,19 @@ import {ActivatedRoute} from '@angular/router';
 export class TopicCreationComponent implements OnInit {
 
   formGroup: FormGroup;
-  error: string;
   isNewCollection: boolean;
+
+  /**
+   * The formStatus is used as a helper
+   * to render the spinner, error and info boxes.
+   */
+  protected formProcessing: FormProcessingStatus = new FormProcessingStatus();
+
+  /**
+   * The form containing the default config loaded from the server.
+   * Can be undefined if not supported by the server.
+   */
+  protected defaultConfigForm: XmppDataForm;
 
   constructor(private creationService: TopicCreationService,
               private navigationService: NavigationService,
@@ -20,38 +34,50 @@ export class TopicCreationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.formProcessing.begin();
+    this.creationService.loadDefaultConfig()
+      .then((form) => {
+        this.defaultConfigForm = form;
+        this.formProcessing.done();
+      })
+      .catch(() => {
+        this.formProcessing.done();
+      });
     this.isNewCollection = this.route.snapshot.data.type === 'collection';
     this.formGroup = new FormGroup({
       'nodeID': new FormControl(null)
     });
   }
 
-  submit() {
-    this.formGroup.disable();
-    this.creationService.createTopic(this.formGroup.get('nodeID').value)
+  submit(configElement?: TopicConfigComponent, configData?: XmppDataForm) {
+    if (configElement && !configData) {
+      configData = configElement.createFormToSubmit();
+    }
+    this.formProcessing.begin();
+    this.creationService.createTopic(this.formGroup.get('nodeID').value, configData)
       .then((topicIdentifier) =>
         this.navigationService.goToTopic(topicIdentifier)
       ).catch((error) => {
       this.formGroup.enable();
       switch (error.condition) {
         case TopicCreationErrors.FeatureNotImplemented:
-          this.error = 'Service does not support node creation';
+          this.formProcessing.done({errorMessage: 'Service does not support node creation'});
           break;
         case TopicCreationErrors.RegistrationRequired:
-          this.error = 'Service requires registration';
+          this.formProcessing.done({errorMessage: 'Service requires registration'});
           break;
         case TopicCreationErrors.Forbidden:
-          this.error = 'Requesting entity is prohibited from creating nodes';
+          this.formProcessing.done({errorMessage: 'Requesting entity is prohibited from creating nodes'});
           break;
         case TopicCreationErrors.Conflict:
-          this.error = 'A topic with the given identifier does already exist';
+          this.formProcessing.done({errorMessage: 'A topic with the given identifier does already exist'});
           break;
         case TopicCreationErrors.NodeIdRequired:
-          this.error = 'Service does not support instant nodes';
+          this.formProcessing.done({errorMessage: 'Service does not support instant nodes'});
           break;
         default:
           console.log(error);
-          this.error = `Failed to create new topic: (${error.type}: ${error.condition})`;
+          this.formProcessing.done({errorMessage: `Failed to create new topic: (${error.type}: ${error.condition})`});
       }
     });
     return false;
