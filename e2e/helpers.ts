aa0d1a862a3a333aa03ee54cfb8708661cbd2611
Fabian Hauser <fabian@fh2.ch>
2018-05-05 12:10:36 +0200

    Add timeout to give spinner some time to initalise

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -4,9 +4,14 @@ export class Helpers {
   /**
    * Returns promise that resolves as soon as no `xgb-spinner`-tags are visible.
    */
-  static noSpinners(retryMs?: number = 5000): Promise<void> {
+  static noSpinners(retryMs?: number = 20000): Promise<void> {
     const EC = protractor.ExpectedConditions;
-    const spinner = $('xgb-spinner');
-    return browser.wait(EC.not(EC.presenceOf(spinner)), retryMs);
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const spinner = $('xgb-spinner');
+        browser.wait(EC.not(EC.presenceOf(spinner)), retryMs)
+          .then(() => resolve());
+      }, 5000); // wait a second to give the spinner time to initialise
+    });
   }
 }
