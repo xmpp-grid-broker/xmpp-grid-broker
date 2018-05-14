b9d3c017a8bb9274d3948a31815f066e33cb56ad
Fabian Hauser <fabian@fh2.ch>
2018-05-14 14:23:51 +0200

    Update script to include e2e openfire in tls cert updates

--- stack/add_certs_to_openfire.bash
+++ stack/add_certs_to_openfire.bash
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-export PASS="changeit"
+###############################################################################
+# Environment
+###############################################################################
+export KEYSTORE_PASS="changeit"
+ROOTDIR=../
+KEYDIR=${ROOTDIR}stack/certs/keys/
+
+if [ "$UID" == "" ]; then
+  UID=`id -u`
+fi
 
 
+###############################################################################
+# Functions & Aliases
+###############################################################################
 header() {
     echo -e "\n\033[1m"
     echo "$1"
@@ -10,36 +22,41 @@ header() {
     echo -e -n "\033[0m"
 }
 
+alias keytool="docker run -i --entrypoint keytool --volume ${ROOTDIR}:/mnt/ --workdir /mnt --user $UID openjdk:8-jdk-alpine"
 
-if [ "$UID" == "" ]; then
-  UID=`id -u`
-fi
-
-alias keytool="docker run -i --entrypoint keytool --volume ./:/mnt/ --workdir /mnt --user $UID openjdk:8-jdk-alpine"
-
-deletealiasfrom() {
-  keytool -delete -keystore $2 -storepass "${PASS}" -alias "$1" || echo "Note: There was no key named '$1' in '$2'."
+deleteAliasFrom() {
+  keytool -delete -keystore $2 -storepass "${KEYSTORE_PASS}" -alias "$1" || echo "Note: There was no key named '$1' in '$2'."
 }
 
-header "Import CA in client.truststore"
-deletealiasfrom "testca" openfire/conf/security/client.truststore
-keytool -importcert -storepass "${PASS}" -keystore openfire/conf/security/client.truststore -alias testca -file certs/keys/ca-fullchain.pem
+setupOpenfire() {
+  OPENFIRE_CONF=$1
 
+  header "Import CA in ${OPENFIRE_CONF}client.truststore"
+  deleteAliasFrom "testca" ${OPENFIRE_CONF}client.truststore
+  keytool -importcert -storepass "${KEYSTORE_PASS}" -keystore ${OPENFIRE_CONF}client.truststore -alias testca -file ${KEYDIR}ca-fullchain.pem
+
+
+  header "Clean out old host certificates in ${OPENFIRE_CONF}keystore"
+  for name in openfire openfire_rsa openfire_dsa rsa dsa; do
+    deleteAliasFrom "$name" ${OPENFIRE_CONF}keystore
+  done
+
+
+  header "Import new host certificates to ${OPENFIRE_CONF}keystore"
+  keytool -importkeystore -deststorepass "${KEYSTORE_PASS}" \
+    -destkeypass "${KEYSTORE_PASS}" -destkeystore ${OPENFIRE_CONF}keystore\
+    -srckeystore ${KEYDIR}host/openfire/full.p12 -srcstoretype PKCS12 -srcstorepass "${KEYSTORE_PASS}"\
+    -alias openfire
+}
+
+###############################################################################
+# Main Script
+###############################################################################
 
 header "Convert host certificate"
-openssl pkcs12 -export -in certs/keys/host/openfire/fullchain.pem -inkey certs/keys/host/openfire/privkey.pem\
-  -out certs/keys/host/openfire/full.p12 -name "openfire" -passout "pass:${PASS}"\
-  -CAfile certs/keys/ca-fullchain.pem -caname root && echo "Success!"
+openssl pkcs12 -export -in ${KEYDIR}host/openfire/fullchain.pem -inkey ${KEYDIR}host/openfire/privkey.pem\
+  -out ${KEYDIR}host/openfire/full.p12 -name "openfire" -passout "pass:${KEYSTORE_PASS}"\
+  -CAfile ${KEYDIR}ca-fullchain.pem -caname root && echo "Success!"
 
-
-header "Clean out old host certificates"
-for name in openfire openfire_rsa openfire_dsa rsa dsa; do
-  deletealiasfrom "$name" openfire/conf/security/keystore
-done
-
-
-header "Import new host certificates"
-keytool -importkeystore -deststorepass "${PASS}" \
-  -destkeypass "${PASS}" -destkeystore openfire/conf/security/keystore\
-  -srckeystore certs/keys/host/openfire/full.p12 -srcstoretype PKCS12 -srcstorepass "${PASS}"\
-  -alias openfire
+setupOpenfire "${ROOTDIR}stack/openfire/conf/security/"
+setupOpenfire "${ROOTDIR}e2e/stack/openfire/conf/security/"
